058394fd59dbab39650591a2db7e3bc47b636294
acer <acer@boss>
2018-07-07 09:27:11 +0530

    json and server thing

---
+++ jsfile.js
@@ -0,0 +1,19 @@
+"use strict"
+function getFile(file,callback){
+    var xhr=new XMLHttpRequest();
+    xhr.overrideMimeType("application/json");
+    xhr.open("GET",file,true);
+    xhr.onreadystatechange=function(){
+        if(xhr.readyState===4 && xhr.status == "200"){
+            callback(xhr.responseText);
+        }
+    };
+    xhr.send(null);
+}
+
+getFile("text.json",function(text){
+ let data=JSON.parse(text);
+ console.log(data);
+
+}
+)
